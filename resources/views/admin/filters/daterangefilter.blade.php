a4a77011df803799a3e4a1924ac6a6397eec00f5
Devcharmi <devcharmi@gmail.com>
2026-02-09 15:30:30 +0530

    filter modal set common

--- resources/views/admin/filters/daterangefilter.blade.php
+++ resources/views/admin/filters/daterangefilter.blade.php
@@ -1,7 +1,19 @@
-<div class="mb-2 d-flex align-items-center justify-content-end">
+{{-- <div class="mb-2 d-flex align-items-center justify-content-end">
     <label for="dateRange" class="form-label me-2 mb-0 fw-bold">Select Date Range </label>
     <input type="text" id="dateRange" class="form-control date-range applyFilters me-3" style="width: 250px;">
     <button type="button" id="resetFilters" class="btn btn-secondary ms-1">
         <i class="fa fa-undo"></i> Reset Filters
     </button>
+</div> --}}
+<div class="row align-items-end mb-2">
+    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <label for="input-label" class="form-label">Date Range</label>
+        <input type="text" id="dateRange" class="form-control date-range applyFilters" autocomplete="off">
+    </div>
+
+    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 d-flex justify-content-end mt-2 mt-md-0">
+        <button type="button" id="resetFilters" class="btn btn-secondary">
+            <i class="fa fa-undo me-1"></i> Reset Filters
+        </button>
+    </div>
 </div>
